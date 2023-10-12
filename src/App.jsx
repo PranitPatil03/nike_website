ab1a9fff99a0f5b04f6a9218defe90efdb694408
Pranit <patilpranit3112@gmail.com>
2023-10-12 13:56:21 +0530

    Project Structure Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {
+  Footer,
+  Services,
+  Subscribe,
+  PopularProducts,
+  Hero,
+  CustomerReviews,
+  SuperQuality,
+  SpecialOffer,
+} from "./sections/index";
+
+import Nav from "./components/Nav";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>Hello</div>
+      <main className="relative">
+        <Nav />
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          <Hero />
+        </section>
+        <section className="padding">
+          <PopularProducts />
+        </section>
+        <section className="padding">
+          <SuperQuality />
+        </section>
+        <section className="padding-x py-10">
+          <Services />
+        </section>
+        <section className="padding">
+          <SpecialOffer />
+        </section>
+        <section className="padding bg-pale-blue">
+          <CustomerReviews />
+        </section>
+        <section className="padding-x sm:py-32 py-16 w-full">
+          <Subscribe />
+        </section>
+        <section className="bg-black padding-x padding-t pb-8">
+          <Footer />
+        </section>
+      </main>
     </>
   );
 }
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+
+export {
+  Footer,
+  Services,
+  Subscribe,
+  PopularProducts,
+  Hero,
+  CustomerReviews,
+  SuperQuality,
+  SpecialOffer,
+};
